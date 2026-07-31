6b8ce90cbb8c8f5b50a84ba1b4c670ad20ca60f1
ip <alpanilya@gmail.com>
2026-07-31 22:17:00 +0700

    Создание Web Razor проекта

---
+++ MafiaManager.Web/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MafiaManager.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
